a3d76d381faa35dc859c017eb56ceb4217eff22d
nareiqat <nabeelareiqat@gmail.com>
2021-03-10 21:45:41 -0500

    fixed about me

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 // typed text animation
 
 var typed = new Typed('.typed-text', {
-  strings: ["Web Developer", "Engineer", "Student"],
+  strings: ["Web Developer", "Mechanical Engineer", "Student"],
   typeSpeed: 120,
   backSpeed: 100,
   backDelay:700,
@@ -20,10 +20,37 @@ var typed = new Typed('.typed-text', {
 });
 
 var typed = new Typed('.typed-2', {
-  strings: ["Web Developer", "Engineer", "Student"],
+  strings: ["Web Developer", "Mechanical Engineer", "Student"],
   typeSpeed: 120,
   backSpeed: 100,
   backDelay:700,
   loop:true,
 
 });
+
+// card flip animation
+
+$(function() {
+  var front = document.getElementsByClassName("front");
+  var back = document.getElementsByClassName("back");
+
+  var highest = 0;
+  var absoluteSide = "";
+
+  for (var i = 0; i < front.length; i++) {
+    if (front[i].offsetHeight > back[i].offsetHeight) {
+      if (front[i].offsetHeight > highest) {
+        highest = front[i].offsetHeight;
+        absoluteSide = ".front";
+      }
+    } else if (back[i].offsetHeight > highest) {
+      highest = back[i].offsetHeight;
+      absoluteSide = ".back";
+    }
+  }
+  $(".front").css("height", highest);
+  $(".back").css("height", highest);
+  $(absoluteSide).css("position", "absolute");
+});
+
+Resources
