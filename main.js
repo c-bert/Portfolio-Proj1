98a99a8dc5d99578f6099479e3b0b1f9378dd0b8
nareiqat <nabeelareiqat@gmail.com>
2021-03-16 19:34:47 -0400

    fixing...

--- main.js
+++ main.js
@@ -1,35 +1,15 @@
-// $(function(){
-//   $(window).scroll(function(){
-//     if(this.scrollY > 20){
-//       $('.navbar').addClass("sticky");
-//     }else{
-//       $('.navbar').removeClass("sticky")
-//     }
-//   })
-// })
-
-// typed text animation
 
 
-function typing(){
+$(function typing() {
   var typed = new Typed('.typed-text', {
-  strings: ["Web Developer", "Mechanical Engineer", "Student"],
-  typeSpeed: 120,
-  backSpeed: 100,
-  backDelay:700,
-  loop:true,
+    strings: ["Web Developer", "Mechanical Engineer", "Student"],
+    typeSpeed: 120,
+    backSpeed: 100,
+    backDelay: 700,
+    loop: true,
 
+  });
 });
-
-  // var typed = new Typed('.typed-2', {
-  //   strings: ["Web Developer", "Mechanical Engineer", "Student"],
-  //   typeSpeed: 120,
-  //   backSpeed: 100,
-  //   backDelay:700,
-  //   loop:true,
-
-  // });
-}
 
 
 // card flip animation
@@ -59,31 +39,31 @@ $(function cardFlip() {
 
 // validate contact form
 function validateForm() {
-  var name =  document.getElementById('name').value;
+  var name = document.getElementById('name').value;
   if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
-      return false;
+    document.querySelector('.status').innerHTML = "Name cannot be empty";
+    return false;
   }
-  var email =  document.getElementById('email').value;
+  var email = document.getElementById('email').value;
   if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
-      return false;
+    document.querySelector('.status').innerHTML = "Email cannot be empty";
+    return false;
   } else {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
-          document.querySelector('.status').innerHTML = "Email format invalid";
-          return false;
-      }
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email)) {
+      document.querySelector('.status').innerHTML = "Email format invalid";
+      return false;
+    }
   }
-  var subject =  document.getElementById('subject').value;
+  var subject = document.getElementById('subject').value;
   if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
-      return false;
+    document.querySelector('.status').innerHTML = "Subject cannot be empty";
+    return false;
   }
-  var message =  document.getElementById('message').value;
+  var message = document.getElementById('message').value;
   if (message == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
-      return false;
+    document.querySelector('.status').innerHTML = "Message cannot be empty";
+    return false;
   }
   document.querySelector('.status').innerHTML = "Sending...";
 }
@@ -94,21 +74,14 @@ $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
 
 // detect scroll top or down
 if ($('.smart-scroll').length > 0) { // check if element exists
-  console.log('hi like')
-    var last_scroll_top = 0;
-    $(window).on('scroll', function() {
-        scroll_top = $(this).scrollTop();
-        if(scroll_top < last_scroll_top) {
-            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-        }
-        else {
-            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-        }
-        last_scroll_top = scroll_top;
-    });
+  var last_scroll_top = 0;
+  $(window).on('scroll', function () {
+    scroll_top = $(this).scrollTop();
+    if (scroll_top < last_scroll_top) {
+      $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+    } else {
+      $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+    }
+    last_scroll_top = scroll_top;
+  });
 }
-
-$(function() {
-  typing();
-  cardFlip();
-});
